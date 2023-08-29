7fd2a201092ca5f074aefb1a32d098e743aa268d
Patrick Hermann <patrick.hermann@sva.de>
2023-08-29 11:56:20 +0000

    updated sthingsBase 23.0829.1156 for tag version v0.1.17

--- filesystem.go
+++ filesystem.go
@@ -18,3 +18,25 @@ func CreateNestedDirectoryStructure(directoryStructure string, permission int) {
 	}
 
 }
+
+func VerifyFileExistence(filePath, kind string, log *Logger, exitOnError bool) bool {
+
+	exists := false
+
+	fileExists, _ := VerifyIfFileOrDirExists(filePath, kind)
+	if !fileExists {
+		log.Warn(kind + " " + filePath + " was not found")
+
+		if exitOnError {
+			log.Error("exiting.. goodbye galaxy!")
+			os.Exit(3)
+		}
+
+	} else {
+		log.Info(kind + " " + filePath + " exists")
+		exists = true
+	}
+
+	return exists
+
+}
